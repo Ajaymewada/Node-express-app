bb63eebe032f228fd8f8c6380d0678ebf52ea36f
ajay <ajaymevda999@gmail.com>
2023-02-16 22:58:07 +0530

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-// const http = require('http')
+const http = require('http')
 // const hostname = '127.0.0.1';
 // const port = 3000;
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World! NodeJS \n');
-// });
+http.createServer((req, res) => {
+  res.write("Hello Ajay");
+  res.end()
+}).listen(80)
 
 // server.listen(port, hostname, () => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-import express from "express";
-var app = express();
+// import express from "express";
+// var app = express();
 
-app.get("/",(req,res) => {
-    res.send("Hey Hi!!")
-})
+// app.get("/",(req,res) => {
+//     res.send("Hey Hi!!")
+// })
 
-app.listen(80,() => {
-  console.log("Server is running on port number 80");
-})
+// app.listen(80,() => {
+//   console.log("Server is running on port number 80");
+// })
