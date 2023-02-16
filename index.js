bf522afdd71ca0a57b4cc736c1c76b3aecaf3baf
ajay <ajaymevda999@gmail.com>
2023-02-16 22:26:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// const http = require('http')
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World! NodeJS \n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+import express from "express";
+var app = express();
+
+app.get("/",(req,res) => {
+    res.send("Hey Hi!!")
+})
+
+app.listen(5001,() => {
+  console.log("Server is running on port number 5001");
+})
